3141815b1405043061f7be12c7d5b138e6e91966
Abhiraman Kuntimaddi <abhiraman21696@icloud.com>
2024-01-11 19:22:09 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import SpotifyNowPlaying from './SpotifyNowPlaying'
+
+function App() {
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen bg-black">
+      <SpotifyNowPlaying />
+    </div>
+  )
+}
+
+export default App
