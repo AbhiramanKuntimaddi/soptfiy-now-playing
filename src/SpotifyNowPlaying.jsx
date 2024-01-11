ef954322add51fb39f32eb92742cf6146633184a
Abhiraman Kuntimaddi <abhiraman21696@icloud.com>
2024-01-11 23:30:30 +0100

    Revering back!

--- src/SpotifyNowPlaying.jsx
+++ src/SpotifyNowPlaying.jsx
@@ -7,19 +7,17 @@ import { average, prominent } from "color.js";
 const SpotifyNowPlaying = (props) => {
 	const [loading, setLoading] = useState(true);
 	const [result, setResult] = useState({});
-	const [backgroundColor, setBackgroundColor] = useState("bg-gray-dark"); // Default background color
 
-	// Watch for changes in the 'result' state
-	useEffect(() => {
-		if (result.isPlaying) {
-			// Extract color information
-			const avgColor = average(result.albumImageUrl, { format: "hex" });
-			const domColor = prominent(result.albumImageUrl, { amount: 1 });
+	if(result.isPlaying){
+		//console.log(result.albumImageUrl);
+		// const options = {
+		// 	crossOrigin : 'Anonymous'
+		// }
+		// const colors = extractColors(result.albumImageUrl, options).then(console.log).catch(console.error);
+		const avgColor = average(result.albumImageUrl, {format: 'hex'});
+		const domColor = prominent(result.albumImageUrl, {amount: 1});	
 
-			// Set the background color dynamically
-			setBackgroundColor(`bg-${avgColor}`);
-		}
-	}, [result]);
+	}
 
 	useEffect(() => {
 		Promise.all([
@@ -32,24 +30,29 @@ const SpotifyNowPlaying = (props) => {
 			setResult(results[0]);
 			setLoading(false);
 		});
-	}, []);
+	});
 
 	return (
 		<div
-			className={`flex min-w-md w-[250px] h-fit text-white items-center justify-center p-2 text-base rounded-lg border-gray-dark ${backgroundColor}`}>
+			className="flex
+         min-w-md w-[250px] h-fit text-white items-center justify-center p-2  text-base rounded-lg border-gray-dark bg-gray-dark">
 			{loading ? (
-				<div className="flex p-2 w-fit items-center justify-center text-gray text-base">
+				<div
+					className="flex p-2
+         w-fit items-center justify-center text-gray  text-base">
 					<img
 						className="w-11 p-1 object-contain h-11 mr-10"
 						src={spotify}
 						alt=""
 					/>
-					<p className="tracking-wider px-1 text-base text-left">Loading...</p>
+					<p className="tracking-wider px-1 text-base text-left ">Loading...</p>
 				</div>
 			) : (
-				<div className="flex p-2 w-fit text-base h-full items-center justify-center">
+				<div
+					className="flex p-2
+         w-fit  text-base h-full items-center justify-center">
 					{result.isPlaying ? (
-						<div className="w-fit h-full py-2 flex flex-col text-left justify-around">
+						<div className=" w-fit  h-full py-2 flex flex-col   text-left justify-around">
 							<div className="w-full h-fit relative">
 								<img
 									className="w-fit h-6 p-1 object-contain absolute -rotate-45 z-10"
@@ -63,10 +66,10 @@ const SpotifyNowPlaying = (props) => {
 								/>
 							</div>
 
-							<div className="flex flex-col text-left mt-2">
+							<div className="flex flex-col  text-left mt-2">
 								<div className="w-fit h-fit">
 									<a
-										className="text-left underline"
+										className="text-left underline "
 										href={result.songUrl}
 										target="_blank"
 										rel="noreferrer">
@@ -74,7 +77,7 @@ const SpotifyNowPlaying = (props) => {
 									</a>
 								</div>
 								<div className="w-fit h-fit">
-									<p className="text-left">{result.artist}</p>
+									<p className="text-left ">{result.artist}</p>
 								</div>
 							</div>
 						</div>
