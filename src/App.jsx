5397a698635076bc376c29dd2f5e07da5dac0568
Abhiraman Kuntimaddi <abhiraman21696@icloud.com>
2024-01-12 16:31:10 +0100

    Added try-catch for better handling!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,60 @@
-import SpotifyNowPlaying from './SpotifyNowPlaying';
-import getNowPlayingItem from './SpotifyAPI';
-import { useEffect, useState } from 'react';
-import { average } from 'color.js';
+import SpotifyNowPlaying from "./SpotifyNowPlaying";
+import getNowPlayingItem from "./SpotifyAPI";
+import { useEffect, useState } from "react";
+import { average } from "color.js";
 
 function App(props) {
-  const [loading, setLoading] = useState(true);
-  const [result, setResult] = useState({});
-  const [avgColor, setAvgColor] = useState("#000000"); // Initial color, you can set it to any default color.
+	const [loading, setLoading] = useState(true);
+	const [result, setResult] = useState({});
+	const [avgColor, setAvgColor] = useState("#000000"); // Initial color, you can set it to any default color.
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const [nowPlayingResult] = await Promise.all([
-          getNowPlayingItem(
-            props.client_id,
-            props.client_secret,
-            props.refresh_token
-          ),
-        ]);
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const [nowPlayingResult] = await Promise.all([
+					getNowPlayingItem(
+						props.client_id,
+						props.client_secret,
+						props.refresh_token
+					),
+				]);
 
-        setResult(nowPlayingResult);
-        setLoading(false);
-      } catch (error) {
-        console.error(error);
-        setLoading(false);
-      }
-    };
+				setResult(nowPlayingResult);
+				setLoading(false);
+			} catch (error) {
+				console.error(error);
+				setLoading(false);
+			}
+		};
 
-    fetchData();
-  });
+		fetchData();
+	});
 
-  useEffect(() => {
-    const updateColor = async () => {
-          const newAvgColor = await average(result.albumImageUrl, {
-            format: 'hex',
-          });
-          setAvgColor(newAvgColor);
-          console.log(newAvgColor);
-    };
+	useEffect(() => {
+		const updateColor = async () => {
+			try {
+				if (result.isPlaying) {
+					const newAvgColor = await average(result.albumImageUrl, {
+						format: "hex",
+					});
+					setAvgColor(newAvgColor);
+					console.log(newAvgColor);
+				}
+			} catch (error) {
+				console.error(error);
+			}
+		};
 
-    updateColor();
-  });
+		updateColor();
+	});
 
-  return (
-    <div
-      className="flex flex-col items-center justify-center w-full h-screen"
-      style={{ backgroundColor: avgColor }}
-    >
-      <SpotifyNowPlaying />
-    </div>
-  );
+	return (
+		<div
+			className="flex flex-col items-center justify-center w-full h-screen"
+			style={{ backgroundColor: avgColor }}>
+			<SpotifyNowPlaying />
+		</div>
+	);
 }
 
 export default App;
